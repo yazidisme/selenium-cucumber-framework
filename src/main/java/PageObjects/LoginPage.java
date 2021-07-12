602eb3c1e6f098b9edc12aac0563535027aa1436
Muhammad Yazid <muhammadyazid26@gmail.com>
2021-07-12 12:51:08 +0700

    Page Object Manager

--- src/main/java/PageObjects/LoginPage.java
+++ src/main/java/PageObjects/LoginPage.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-
 public class LoginPage {
 
     public LoginPage(WebDriver webDriver) {
