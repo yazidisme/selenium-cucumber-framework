05daa4b60ba6abfb98de8d6be55b910c382e8032
Muhammad Yazid <muhammadyazid26@gmail.com>
2021-07-14 14:11:26 +0700

    Maps in Data Tables

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -8,6 +8,7 @@ import io.cucumber.java.en.When;
 import org.testng.Assert;
 
 import java.util.List;
+import java.util.Map;
 
 public class LoginSteps {
 
@@ -36,14 +37,28 @@ public class LoginSteps {
         System.out.println("---" + account);
     }
 
-    @When("Input credentials to login")
-    public void inputCredentialsToLogin(DataTable dataTable) {
+    @When("Input credentials to login without headers")
+    public void inputCredentialsToLoginWithoutHeaders(DataTable dataTable) {
         List<String> dataRow = dataTable.row(0);
         String email = dataRow.get(0);
         System.out.println("row index 0 --- " + email);
         loginPage.fillEmailData(email);
         System.out.println("row index 1 --- " + dataRow.get(1));
         System.out.println("row index 2 --- " + dataRow.get(2));
+    }
+
+    @When("Input credentials to login with headers table")
+    public void inputCredentialsToLoginWithHeadersTable(DataTable dataTable) {
+        List<Map<String,String>> dataRow = dataTable.asMaps(String.class,String.class);
+        //Use for...loop if you have multiple data table
+        for (Map<String, String> dataMap : dataRow) {
+            String email = dataMap.get("Email");
+            loginPage.fillEmailData(email);
+            System.out.println("row header email " + email);
+            System.out.println("row index 1 --- " + dataMap.get("Password"));
+            System.out.println("row index 2 --- " + dataMap.get("Account Type"));
+        }
+
     }
 
     @When("Click selanjutnya button")
--- src/test/resources/features/LoginTests.feature
+++ src/test/resources/features/LoginTests.feature
@@ -4,14 +4,17 @@ Feature: Login tests
   Background: Home page of bhinneka.com
     Given Login form in login page
 
-  @PositiveCase @Staging @Development @DataTable
+  @PositiveCase @Staging @Development @DataTableHeaders
   Scenario: Login to Bhinneka.com using valid account that registered in personal and corporate account type
     Given Login page is displayed
-    When Input credentials to login
-    | your@email.com | Yourp@ssw0rd | personal and corporate |
+    When Input credentials to login with headers table
+    | Email             | Password     | Account Type           |
+    | your@email.com    | Yourp@ssw0rd | personal and corporate |
     And Click selanjutnya button
 
-  @PositiveCase @Staging @Development
+  @PositiveCase @Staging @Development @SimplyDataTable
   Scenario: Login to Bhinneka.com using valid account that registered in personal account type
-    When Login page is displayed
-    And Input "your@email.com" as email, "Yourp@ssw0rd" as password, "personal" as account type
+    Given Login page is displayed
+    When Input credentials to login without headers
+      | your@email.com | Yourp@ssw0rd | personal |
+    And Click selanjutnya button
